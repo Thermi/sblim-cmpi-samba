389d87d01e91ea5d560edac3102853053dc1de8a
Jeremy Bongio <jbongio@us.ibm.com>
2009-09-15 02:06:13 -0400

    Change licensing from CPL to EPL

--- provider/Linux_SambaElementConformsToProfile/Linux_ElementConformsToProfile_Resource.h
+++ provider/Linux_SambaElementConformsToProfile/Linux_ElementConformsToProfile_Resource.h
@@ -1,11 +1,11 @@
 // ============================================================================
 // Copyright © 2007, International Business Machines
 //
-// THIS FILE IS PROVIDED UNDER THE TERMS OF THE COMMON PUBLIC LICENSE
+// THIS FILE IS PROVIDED UNDER THE TERMS OF THE ECLIPSE PUBLIC LICENSE
 // ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS FILE
 // CONSTITUTES RECIPIENTS ACCEPTANCE OF THE AGREEMENT.
 //
-// You can obtain a current copy of the Common Public License from
+// You can obtain a current copy of the Eclipse Public License from
 // http://oss.software.ibm.com/developerworks/opensource/license-cpl.html
 //
 // Authors:             Sam Kannan <samkannan (at) us.ibm.com>
--- provider/Linux_SambaForceGroupForShare/Linux_SambaForceGroupForShareInterface.h
+++ provider/Linux_SambaForceGroupForShare/Linux_SambaForceGroupForShareInterface.h
@@ -2,13 +2,13 @@
 // Linux_SambaForceGroupForShareInterface.h
 //     created on Fri, 23 Jun 2006 using ECUTE 2.2.1
 //
-// Copyright (c) 2006, International Business Machines
+// Copyright (c) 2006, 2009, International Business Machines
 //
-// THIS FILE IS PROVIDED UNDER THE TERMS OF THE COMMON PUBLIC LICENSE
+// THIS FILE IS PROVIDED UNDER THE TERMS OF THE ECLIPSE PUBLIC LICENSE
 // ("AGREEMENT"). ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS FILE
 // CONSTITUTES RECIPIENTS ACCEPTANCE OF THE AGREEMENT.
 //
-// You can obtain a current copy of the Common Public License from
+// You can obtain a current copy of the Eclipse Public License from
 // http://oss.software.ibm.com/developerworks/opensource/license-cpl.html
 //
 // Author:        generated
